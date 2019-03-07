b355d8137924615b12325d56d9f4cd1ef62baf4f
Matin <mnm.maitn@gmail.com>
2019-03-07 14:52:14 +0000

    comments added

--- StudentClass.java
+++ StudentClass.java
@@ -13,22 +13,23 @@ public class StudentClass {
 	}
 	
 	public static int[] computePrefixFunction(String pattern) {
-		
+		//this function returns a pi array called "pei"
+		//pattern is matched against the pattern
 		int k;
 		int m=pattern.length();
 		int pei[];
-		pei=new int[m];
-		pei[0]=0;	//general case
+		pei=new int[m];						
+		pei[0]=0;					//general case
 		k=0;
 									
-		for (int q=2; q<=m;q++) {
+		for (int q=2; q<=m;q++) {		//-1 due to charAt starting from 0
 			while (k>0 && pattern.charAt(k+1-1)!=pattern.charAt(q-1)) {
-				k=pei[k-1];
+				k=pei[k-1];		//starting arrays from 0 so -1 while using arrays
 			}
 			if (pattern.charAt(k+1-1)==pattern.charAt(q-1)) {
 					k=k+1;
 					}
-				pei[q-1]=k;
+				pei[q-1]=k;			//stores max value of k
 			}
 		return pei;	
 	}
@@ -69,12 +70,15 @@ public class StudentClass {
 		}
 
 		public void search() {
-			int n=this.textLen;
+			//the pattern is used with the pei array from computePrefixFunction()
+			//pattern is compared against the text
+			//queue can be returned using getMatchIndices()
+			int n=this.textLen;		
 			int m=this.patternLen;
 			int pei[]= computePrefixFunction(this.pattern);
 			int q=0;
 			
-			for(int i=1;i<=n;i++) {
+			for(int i=1;i<=n;i++) {			//-1 due to charAt starting from 0
 				while(q>0 && this.pattern.charAt(q+1-1)!=this.text.charAt(i-1)) {
 					q=pei[q-1];
 				}
@@ -82,7 +86,7 @@ public class StudentClass {
 					q=q+1;
 				}
 				if (q==m) {
-					this.matchIndices.enqueue(i-m);
+					this.matchIndices.enqueue(i-m);		//enqueue adds to right
 					q=pei[q-1];
 				}
 			}
